66e5fb4c7a5ab49451c7f13ed163cbf2a9134167
owls4all <owls4all@gmail.com>
2024-10-21 11:11:34 -0700

    tested image display

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-import PIL
 import turtle
+#Adobe images should be 9x9 in before converting
+#GIMP images should be 652x652 pixels before converting
 
+turtle.hideturtle()
+turtle.bgpic("take2.gif")
+
+
+turtle.exitonclick()
