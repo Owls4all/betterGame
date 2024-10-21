2c40af6aebbb463e76d7be61481111d62be56cfe
owls4all <owls4all@gmail.com>
2024-10-21 11:45:01 -0700

    did stuff

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import turtle
 #Adobe images should be 9x9 in before converting
 #GIMP images should be 652x652 pixels before converting
-
+first = 'take2.gif'
+second = 'hallTjunction.gif'
 turtle.hideturtle()
-turtle.bgpic("take2.gif")
+turtle.bgpic(first)
+wait = input("type something to proceed")
+turtle.bgpic(second)
+    
 
 
 turtle.exitonclick()
