c6f35301d76da23e58a3f9886e25e32ff8173260
owls4all <owls4all@gmail.com>
2024-10-31 17:01:23 -0700

    potato

--- main.py
+++ main.py
@@ -1,13 +1,2 @@
 import turtle
-#Adobe images should be 9x9 in before converting
-#GIMP images should be 652x652 pixels before converting
-first = 'take2.gif'
-second = 'hallTjunction.gif'
-turtle.hideturtle()
-turtle.bgpic(first)
-wait = input("type something to proceed")
-turtle.bgpic(second)
-    
-
-
-turtle.exitonclick()
+import tkinter
