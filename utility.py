7dbde76cbf416ba1902f9032b99b163e15f4e4af
owls4all <owls4all@gmail.com>
2024-11-04 09:11:06 -0800

    added searchList

--- utility.py
+++ utility.py
@@ -5,6 +5,16 @@ def makeRadians(angle):
     return pi*(angle/180)
 def string_angle(angle):
     return f"{angle/pi} pi"
-print(makeDegrees(pi))
-print(string_angle(makeRadians(180)))
+def ask(prompt):
+    return(input(prompt+'\n>>> '))
+def searchList(quarry,list):
+    for item in list:
+        if item == quarry:
+            return True
+    return False
 
+list = [4,7,'penguin','potato','bicycle']
+if searchList('potato',list):
+    print('dinosaur')
+else:
+    print('something is wrong')
