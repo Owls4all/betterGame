f3d46612c378624890e772b130242338a77d19be
owls4all <owls4all@gmail.com>
2024-11-04 12:26:58 -0800

    added menu stuff

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
-import turtle
-import tkinter
-import utility
+import turtle 
+from tkinter import *
+import utility 
+import time
+
+
+core = Tk()
+core.title('I can set text here aparently')
+
+menubar = Menu(core)
+#------------------Game Menu------------------#
+game = Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Game',menu=game)
+'[Other Game menu commands here]'
+game.add_command(label='Quit',command=core.destroy)
+#----------------Actions Menu-----------------#
+actions = Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Actions',menu = actions)
+actions.add_command(label='Attack', command = None)
+
+#-----------------Magic Menu------------------#
+magic = Menu(menubar,tearoff=0)
+menubar.add_cascade(label='Magic',menu=magic)
+
+core.config(menu = menubar)
+mainloop()
+
