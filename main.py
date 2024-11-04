c03be990f93371611f597cda5f55fdfe49c0c737
owls4all <owls4all@gmail.com>
2024-11-04 13:00:55 -0800

    added more menu items

--- main.py
+++ main.py
@@ -5,22 +5,28 @@ import time
 
 
 core = Tk()
-core.title('I can set text here aparently')
+core.title('Super epic game (definitely no exaggeration whatsoever)')
 
 menubar = Menu(core)
 #------------------Game Menu------------------#
 game = Menu(menubar,tearoff=0)
-menubar.add_cascade(label='Game',menu=game)
-'[Other Game menu commands here]'
-game.add_command(label='Quit',command=core.destroy)
+menubar.add_cascade(label='Game', menu = game)
+game.add_command(label='Save', command = None)
+game.add_command(label='Load', command = None)
+game.add_command(label='Quit', command = core.destroy)
+#---------------------------------------------#
+
 #----------------Actions Menu-----------------#
 actions = Menu(menubar,tearoff=0)
-menubar.add_cascade(label='Actions',menu = actions)
-actions.add_command(label='Attack', command = None)
+menubar.add_cascade(label= 'Actions', menu = actions)
+actions.add_command(label= 'Attack', command = None)
+actions.add_command(label= 'Focus', command = None)
+#---------------------------------------------#
 
 #-----------------Magic Menu------------------#
 magic = Menu(menubar,tearoff=0)
 menubar.add_cascade(label='Magic',menu=magic)
+#---------------------------------------------#
 
 core.config(menu = menubar)
 mainloop()
