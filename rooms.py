4137a91f3cb6b8424cb9f6a66f3fc2b99951c3a5
owls4all <owls4all@gmail.com>
2024-11-05 12:25:02 -0800

    idk

--- rooms.py
+++ rooms.py
@@ -1,7 +1,12 @@
+import random as r
+import utility
 class Room:
-    def __init__(self,east,west,north,south):
-        self.e=east
-        self.w=west
-        self.n=north
-        self.s=south
+    def __init__(self,level):
+        self.level=level
+    def orient(self,entry):
+        if entry == 'north':
+            self.exits =[self.south,self.north,self.west,self.east]
+#rooms have 'forward, back, left, right' -> this is determined by player's point of entry
+# they also have 'north south west east' -> this is absolute direction
 
+steps0=Room(0)
